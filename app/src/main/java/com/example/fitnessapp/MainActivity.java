9f8bdd6c37f76ce79cbb3798708dffde18c6b0ef
Dawid-i <ilczukdawid5@gmail.com>
2025-05-20 09:19:30 +0100

    ye

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button next_screen_btn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
